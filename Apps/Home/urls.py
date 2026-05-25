fa2eba72c3714707c0a7b799974f12b4b3a54da3
RicardoVasquez <rvasquezo5@miumg.edu.gt>
2026-05-25 01:25:48 -0600

    pagina de contacto

--- Apps/Home/urls.py
+++ Apps/Home/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
-from Apps.Home.views import HomeView
+from Apps.Home.views import AcercaView, HomeView, SoporteView
 
 app_name = 'Home'
 urlpatterns = [
     path('', HomeView.as_view(), name='homeapp'),
+    path('acerca/', AcercaView.as_view(), name='acerca_app'),
+    path('soporte/', SoporteView.as_view(), name='soporte_app'),
 ]
--- Apps/Home/views.py
+++ Apps/Home/views.py
@@ -4,3 +4,9 @@ from django.views.generic import TemplateView
 # Create your views here.
 class HomeView(TemplateView):
     template_name = 'home.html'
+
+class AcercaView(TemplateView): 
+    template_name = 'acerca.html'
+
+class SoporteView(TemplateView):
+    template_name = 'soporte.html' 
