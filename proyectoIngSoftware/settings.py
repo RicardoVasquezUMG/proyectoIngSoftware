163b52a6899c4e1da920475c4ac2c2c4aa039384
RicardoVasquez <rvasquezo5@miumg.edu.gt>
2026-05-25 23:19:45 -0600

    configuracion BD

--- proyectoIngSoftware/settings.py
+++ proyectoIngSoftware/settings.py
@@ -78,10 +78,15 @@ WSGI_APPLICATION = 'proyectoIngSoftware.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',  
+        'NAME': 'proyectoIngS',                     
+        'USER': 'postgres',                        
+        'PASSWORD': 'admin',               
+        'HOST': 'localhost',                        
+        'PORT': '5432',                            
     }
 }
+
 
 
 # Password validation
